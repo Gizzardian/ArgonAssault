2b56392e6e3f3dc209ffb7e8ae1ae2a915763bab
Jae Taylor <gizzardian@gmail.com>
2019-07-07 21:50:41 -0700

    splash screen, music

---
+++ Assets/MusicSource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicSource : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadLevel", 5f);
+    }
+
+    private void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
