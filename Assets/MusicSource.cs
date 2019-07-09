ec147c8ddfba60313f642f29bf2ff5c9c4ddf6ab
Jae Taylor <gizzardian@gmail.com>
2019-07-08 17:36:43 -0700

    music continues from splash screen to level 1

--- Assets/MusicSource.cs
+++ Assets/MusicSource.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 public class MusicSource : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private void Awake() 
+    {
+        DontDestroyOnLoad(gameObject);  // keeps music playing, small g means instance of an object
+    }
     void Start()
     {
         Invoke("LoadLevel", 5f);
