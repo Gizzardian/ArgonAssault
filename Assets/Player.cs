5045476119ddb00a26a5f32f64f6426f3293faf8
Jae Taylor <gizzardian@gmail.com>
2019-07-09 00:17:12 -0700

    trying vertical movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     [Tooltip("In meters/second")] [SerializeField] float xSpeed = 40f;
     [Tooltip("In meters")] [SerializeField] float xRange = 15f;
 
+    [Tooltip("In meters")] [SerializeField] float xRangeUp = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +22,16 @@ public class Player : MonoBehaviour
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         // https://www.udemy.com/unitycourse2/learn/lecture/8477780#questions
         float xOffset = xThrow * xSpeed * Time.deltaTime;
-        
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
-
         transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+
+        // my try
+        float xThrowUp = CrossPlatformInputManager.GetAxis("Vertical");
+        float xOffsetUp = xThrowUp * xSpeed * Time.deltaTime;
+        float rawXPosUp = transform.localPosition.y + xOffsetUp;
+        float clampedXPosUp = Mathf.Clamp(rawXPosUp, -xRangeUp, xRangeUp);
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedXPosUp, transform.localPosition.z);
 
     }
 }
